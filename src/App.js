ed86513b62a324d1c90e9d97d2c20dca2de60f4b
k-yamaga <91997384+k-yamaga@users.noreply.github.com>
2023-11-22 18:27:03 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { UserProvider } from "./providers/UserProvider";
+import { Router } from "./router/Router";
+import { RecoilRoot } from "recoil";
+export default function App() {
+  return (
+    <RecoilRoot>
+      <UserProvider>
+        <Router />
+      </UserProvider>
+    </RecoilRoot>
+  );
+}
